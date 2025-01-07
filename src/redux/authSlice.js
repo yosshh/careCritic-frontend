8463a9bbef58c50f30f6b8c4ee4d5c902444b9e4
Yash Shukla <yshukla2775@gmail.com>
2025-01-07 09:29:48 +0530

    initailize redux toolkit

---
+++ src/redux/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        loading:false,
+        // user:null
+    },
+    reducers:{
+        // actions
+        
+        
+        setLoading:(state, action) => {
+            state.loading = action.payload;
+            // console.log("inside reducers");
+        },
+        // setUser:(state, action) => {
+        //     // console.log("Payload received in setUser:", action.payload);
+        //     state.user = action.payload;
+        // }
+    }
+});
+export const {setLoading, setUser} = authSlice.actions;
+export default authSlice.reducer;
